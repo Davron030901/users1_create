2b90daf4ab0e4a676fc85ea17949a90df6a9af57
Davron <152484311+Davron030901@users.noreply.github.com>
2024-04-07 10:55:22 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
-import sqlalchemy
+import sqlite3
 
-engine=sqlalchemy.create_engine('sqlite:///movies.db',echo=True)
+connection = sqlite3.connect('users-sqlite.db')
+cursor = connection.cursor()
 
-with engine.connect() as conn:
-    result=conn.execute(sqlalchemy.text("SELECT * FROM Movies"))
-    for row in result:
-        print(row)
+cursor.execute('''CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, email_address TEXT)''')
+
+
+users_to_insert=[('Sarah','Pery','sarah.perry@gmail.com'),('Felix','Martin','felix.martin@gmail'),('John','Patrick','john.patrick@gmail'),('Jessica','Jones','jessica.jones@gmail.com'),('Percy','Colton','percy.colton@gmail.com')]
+
+cursor.executemany('''INSERT INTO Users (first_name,last_name,email_address) VALUES (?,?,?)''',users_to_insert)
+
+cursor.execute('SELECT * FROM Users')
+print(cursor.fetchall())
+connection.commit()
+cursor.close()
